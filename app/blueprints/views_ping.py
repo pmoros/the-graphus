8ac99a8bfa35c06127edf2881f4fd68eb16f5a44
pmoros <pmoros@outlook.com>
2022-05-01 12:30:50 -0500

    fix: message of test (CI test purpose)

--- app/blueprints/views_ping.py
+++ app/blueprints/views_ping.py
@@ -8,5 +8,5 @@ ping = Blueprint("ping", __name__)
 
 @ping.route("/", methods=["GET"])
 def ping_pong():
-    """Ping endpoint, used to know if the app is up."""
+    """Ping endpoint, used to know if the app is up and running."""
     return app.controller_ping.ping()
