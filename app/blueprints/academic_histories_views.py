ca58ea6eb4abab3daf90a97b8ba0641a7bec5c6f
pmoros <pmoros@outlook.com>
2022-05-30 08:10:25 -0500

    chore: secure academic histories endpoint

--- app/blueprints/academic_histories_views.py
+++ app/blueprints/academic_histories_views.py
@@ -4,12 +4,14 @@ from flask import Blueprint, jsonify
 
 import app
 from app.decorators import error_decorator
+from app.decorators import sub_must_match
 from app.utils.constants import SUCCESS_RESPONSE_TAG
 
 academic_histories = Blueprint("academic-histories", __name__)
 
 
 @academic_histories.route("/user/<string:user_sub>", methods=["GET"])
+@sub_must_match
 @error_decorator
 def get_academic_histories_by_user_sub(user_sub):
     """
